20f9ec8b69c775308c4c62f62114fee0d5290ef5
PubCyBerry <kth7186@gmail.com>
2023-05-18 08:29:11 +0000

    refactor: plot with dynamic grid size

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import os
 import os.path as osp
 import pathlib
@@ -13,8 +14,8 @@ mpl.rcParams["animation.embed_limit"] = 2**128
 
 
 def plot_solution(
-    xs: torch.tensor,
-    ts: torch.tensor,
+    xlim: Tuple[float, float],
+    tlim: Tuple[float, float],
     U: torch.tensor,
     U_pred: torch.tensor = None,
     ps: torch.tensor = None,
@@ -22,8 +23,11 @@ def plot_solution(
     title: str = None,
     filename: str = None,
     result_dir: str = "results",
+    figsize=None,
 ):
-    fig, ax = plt.subplots()
+    xs = torch.linspace(*xlim, U.shape[0])
+    ts = torch.linspace(*tlim, U.shape[1])
+    fig, ax = plt.subplots(figsize=figsize)
 
     # Additional margin for title
     extra_top_margin: float = 0
@@ -56,14 +60,14 @@ def plot_solution(
     imshow_config = dict(
         interpolation="nearest",
         cmap="rainbow",
-        extent=[ts.min(), ts.max(), xs.min(), xs.max()],
+        extent=[*tlim, *xlim],
         origin="lower",
         aspect="auto",
     )
 
     # Draw vertical line at time to visualize
     ax = plt.subplot(gs0[0, 0])
-    line = torch.linspace(xs.min(), xs.max(), 2)[:, None]
+    line = torch.linspace(*xlim, 2)[:, None]
     for t in ts[snapshot_idx]:
         ax.plot(t * torch.ones((2, 1)), line, "w-", linewidth=1)
 
@@ -104,15 +108,16 @@ def plot_solution(
         range(ncol), imgs, ax_titles, ax_xlabels, ax_ylabels
     ):
         ax = plt.subplot(gs0[0, col])
-        ax.set_title(ax_title, fontsize=20)
-        ax.set_xlabel(ax_xlabel, fontsize=20)
-        ax.set_ylabel(ax_ylabel, fontsize=20)
+        ax.set_title(ax_title, fontsize=12)
+        ax.set_xlabel(ax_xlabel, fontsize=12)
+        if col == 0:
+            ax.set_ylabel(ax_ylabel, fontsize=15)
         h = ax.imshow(img, **imshow_config)
 
     divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="3%", pad=0.10)
+    cax = divider.append_axes("right", size="6%", pad=0.10)
     cbar = fig.colorbar(h, cax=cax)
-    cbar.ax.tick_params(labelsize=15)
+    cbar.ax.tick_params(labelsize=12)
 
     # Row 1: Plot solution at certain timestamps(t=0.00, 0.25, 0.50, 0.75)(%)
     ncol = len(snapshot_idx)
@@ -123,13 +128,13 @@ def plot_solution(
         ax = plt.subplot(gs1[0, col])
         ax.plot(xs, U[:, idx], "b-", linewidth=2, label="Exact")
         if U_pred is not None:
-            ax.plot(xs, U_pred[:, idx], "r--", linewidth=2, label="Prediction")
+            ax.plot(torch.linspace(*xlim, U_pred.shape[0]), U_pred[:, idx], "r--", linewidth=2, label="Prediction")
         ax.set_xlabel("$x$")
         if col == 0:
             ax.set_ylabel("$u(t,x)$")
         ax.set_title("$t=%.2fs$" % (ts[idx]), fontsize=8)
         ax.axis("square")
-        ax.set_xlim((xs.min(), xs.max()))
+        ax.set_xlim(xlim)
         ax.set_ylim((U.min() - 0.2, U.max() + 0.2))
         ax.tick_params(axis="both", which="both", labelsize=6)
         ax.grid(which="both", axis="both", linestyle="--")
@@ -160,15 +165,18 @@ def plot_solution(
 
 
 def animate_solution(
-    xs: torch.tensor,
+    xlim: Tuple[float, float],
+    tlim: Tuple[float, float],
     ys: torch.tensor,
-    ts: torch.tensor,
     y_pred: torch.tensor = None,
     interval: int = 50,
     fps: int = 60,
     save_img: bool = True,
     filename: str = None,
     result_dir: str = "results",
+    num_x:int = 256,
+    num_t:int = 128,
+    figsize=None
 ):
     """
     plot time evolution animation of solution
@@ -176,10 +184,17 @@ def animate_solution(
     y: u(x,t) <- (num_x, num_t)
     interval: time interval between frames (ms)
     """
+    xs = torch.linspace(*xlim, ys.shape[0])
+    ts = torch.linspace(*tlim, ys.shape[1])
 
+    # Downsample images for speedup
+    fast_x = len(xs) // num_x
+    xs = xs[::fast_x]
+    fast_t = len(ts) // num_t
+    ts = ts[::fast_t]
     dt = ts[1] - ts[0]
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=figsize)
     ax = plt.axes(xlim=(xs.min() - 0.2, xs.max() + 0.2), ylim=(ys.min() - 0.2, ys.max() + 0.2))
     (line,) = ax.plot([], [], "b-", linewidth=2, label="Exact")
     (pred,) = ax.plot([], [], "r--", linewidth=2, label="Prediction")
@@ -188,7 +203,10 @@ def animate_solution(
     ax.set_ylabel(r"$u(x) \ [\mathrm{m}/s]$", fontsize=14)
     ax.grid(which="both", axis="both", linestyle="--")
 
+    ys = ys[::fast_x,::fast_t]
     if y_pred is not None:
+        y_pred = y_pred[::fast_x,::fast_t]
+        pred_xs = torch.linspace(*xlim, y_pred.shape[0])
         ax.legend()
 
     def init():
@@ -203,7 +221,7 @@ def animate_solution(
     def animate(i):
         line.set_data(xs, ys[:, i])
         if y_pred is not None:
-            pred.set_data(xs, y_pred[:, i])
+            pred.set_data(pred_xs, y_pred[:, i])
         ax.set_title(
             f"Waveform $u(x,t)$ at t = {torch.round(dt * i, decimals=2):.2f} seconds",
             fontsize=14,
