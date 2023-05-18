70bcf5a80890e99ab61c36a5a217b493e20c8957
PubCyBerry <kth7186@gmail.com>
2023-05-18 08:29:44 +0000

    feat: add model inferences

--- src/web/app.py
+++ src/web/app.py
@@ -1,42 +1,36 @@
 # Type hint
 # Config
-import os.path as osp
+from pathlib import Path
 from typing import Any, Optional, Tuple
 
 # webapp
 import streamlit as st
-
-# ML backend
-import torch
-from omegaconf import DictConfig, OmegaConf
 from streamlit import session_state as sess
 from streamlit.components import v1 as components
 
-from src.data.create_data import Data_Generator
+# from streamlit_extras import switch_page_button
+
+# ML backend
+import numpy as np
+import torch
+from omegaconf import DictConfig, OmegaConf
+
 
 # user-defined libs
 from src.utils import plotting
-from src.utils.utils import load_checkpoint, make_mesh
-
-# from streamlit_extras import switch_page_button
+from src.utils.utils import load_model, make_mesh
+from src.data.create_data import Data_Generator
 
 
 ############ CONTENTS ###############
-def web_figure(target_func: str, *args: Any, **kwds: Any) -> None:
+def web_figure(target_func: str, *args: Any, **kwds: Any):
     fig = getattr(plotting, target_func)(*args, **kwds)
     st.pyplot(fig)
 
 
-def web_animation(target_func: str, *args: Any, **kwds: Any) -> None:
+def web_animation(target_func: str, *args: Any, **kwds: Any):
     anim = getattr(plotting, target_func)(*args, **kwds)
     components.html(anim.to_jshtml(), width=800, height=550, scrolling=False)
-
-
-def load_model(model_name: str = None, model_dir: str = "model") -> torch.nn.Module:
-    model_cfg: DictConfig = OmegaConf.load(osp.join(model_dir, model_name + ".yaml"))
-    checkpoint_path: str = osp.join(model_dir, model_name + ".pth")
-    model: torch.nn.Module = load_checkpoint(model_cfg, checkpoint_path)
-    return model
 
 
 #####################################
@@ -49,9 +43,7 @@ def main():
     st.title(app_title)
 
     top_container = st.container()
-    top1, top2 = top_container.columns([1, 1])
-    button_data = top1.button("Solve PDE", use_container_width=True)
-    button_draw = top2.button("Plot Solution", use_container_width=True)
+    top1, top2, top3 = top_container.columns([1, 1, 1])
 
     # PDE Configuration
     with st.expander("PDE Configuration"):
@@ -63,20 +55,20 @@ def main():
 
         # kinematic viscosity
         coefficient: float = st.number_input(
-            "kinematic viscosity", value=0.003, format="%.4f", label_visibility="collapsed"
+            "kinematic viscosity", value=0.01, format="%.4f", label_visibility="collapsed"
         )
 
         st.subheader("Spatial Domain")
         x_left, x_right, grid_x = st.columns(3)
         xmin: float = x_left.number_input("Left Boundary(m)", value=0.0)
         xmax: float = x_right.number_input("Right Boundary(m)", value=1.0)
-        Nx: int = grid_x.number_input("Spatial Grids", value=512, key="Nx")
+        Nx: int = grid_x.number_input("Spatial Grids", value=1024, key="Nx")
 
         st.subheader("Temporal Domain")
         t_left, t_right, grid_t = st.columns(3)
         tmin: float = t_left.number_input("Initial Time(s)", value=0.0)
         tmax: float = t_right.number_input("Final Time(s)", value=1.0)
-        Nt: int = grid_t.number_input("Temporal Grids", value=128, key="Nt")
+        Nt: int = grid_t.number_input("Temporal Grids", value=512, key="Nt")
 
         sess.xlim: Tuple[float, float] = (xmin, xmax)
         sess.tlim: Tuple[float, float] = (tmin, tmax)
@@ -102,12 +94,7 @@ def main():
             gamma: float = st.number_input("γ : regularity of random field", 2.5)
             is_parallel: bool = False
             current_variance = (
-                f"$C={int(sigma)}"
-                + r"\left(-\dfrac{d^2}{dx^2}"
-                + rf"+{int(tau)}I \right)"
-                + "^{"
-                + f"{-gamma}"
-                + "}$"
+                f"$C={int(sigma)}" + r"\left(-\dfrac{d^2}{dx^2}" + rf"+{int(tau)}I \right)" + "^{" + f"{-gamma}" + "}$"
             )
 
         with col2:
@@ -118,7 +105,7 @@ def main():
             st.markdown(r"##### Current setting")
             st.markdown(current_variance)
 
-    if button_data:
+    if top1.button("Solve PDE", use_container_width=True):
         with st.spinner("Calculation..."):
             sess.data = sess.generator.create_data(
                 num_data=num_data,
@@ -130,7 +117,13 @@ def main():
             ).squeeze()
             st.success("Calculation Complete", icon="✅")
 
-    if button_draw and "data" in sess:
+    if top2.button("Load default", use_container_width=True):
+        with st.spinner("Loading..."):
+            data = np.load("data/demo.npz")
+            sess.data = torch.Tensor(data["ys"])
+            st.success("Calculation Complete", icon="✅")
+
+    if top3.button("Plot Solution", use_container_width=True) and "data" in sess:
         with st.expander("Solution", expanded=True):
             with st.spinner("Drawing Full Field..."):
                 web_figure("plot_solution", sess.generator.xs, sess.generator.ts, sess.data)
@@ -154,23 +147,16 @@ def main():
         btn.write("\n")
         if btn.button("Infer", use_container_width=True, key=f"button_{model_name}"):
             model = load_model(model_name)
-            data = make_mesh(torch.linspace(*sess.xlim, Nx), torch.linspace(*sess.tlim, Nt))
+            data = make_mesh(torch.linspace(*sess.xlim, Nx), torch.linspace(*sess.tlim, Nt)).cuda()
 
             # Perform inference
-            preds = model(data[:, :1], data[:, 1:]).view(Nx, Nt)
+            preds = model((data[:, :1], data[:, 1:])).view(Nx, Nt).detach().cpu()
 
             # Plot result
             with st.spinner("Drawing Full Field..."):
-                web_figure("plot_solution", sess.generator.xs, sess.generator.ts, sess.data, preds)
+                web_figure("plot_solution", sess.xlim, sess.tlim, sess.data, preds)
             with st.spinner("Animating Solution..."):
-                web_animation(
-                    "animate_solution",
-                    sess.generator.xs,
-                    sess.data,
-                    sess.generator.ts,
-                    preds,
-                    save_img=False,
-                )
+                web_animation("animate_solution", sess.xlim, sess.tlim, sess.data, preds, save_img=False)
         del sess[f"Nx_{model_name}"], sess[f"Nt_{model_name}"], sess[f"button_{model_name}"]
 
     with st.expander("PINN"):
@@ -184,23 +170,16 @@ def main():
         btn.write("\n")
         if btn.button("Infer", use_container_width=True, key=f"button_{model_name}"):
             model = load_model(model_name)
-            data = make_mesh(torch.linspace(*sess.xlim, Nx), torch.linspace(*sess.tlim, Nt))
+            data = make_mesh(torch.linspace(*sess.xlim, Nx), torch.linspace(*sess.tlim, Nt)).float().cuda()
 
             # Perform inference
-            preds = model(data[:, :1], data[:, 1:]).view(Nx, Nt)
+            preds = model((data[:, :1], data[:, 1:])).view(Nx, Nt).detach().cpu()
 
             # Plot result
             with st.spinner("Drawing Full Field..."):
-                web_figure("plot_solution", sess.generator.xs, sess.generator.ts, sess.data, preds)
+                web_figure("plot_solution", sess.xlim, sess.tlim, sess.data, preds)
             with st.spinner("Animating Solution..."):
-                web_animation(
-                    "animate_solution",
-                    sess.generator.xs,
-                    sess.data,
-                    sess.generator.ts,
-                    preds,
-                    save_img=False,
-                )
+                web_animation("animate_solution", sess.xlim, sess.tlim, sess.data, preds, save_img=False)
         del sess[f"Nx_{model_name}"], sess[f"Nt_{model_name}"], sess[f"button_{model_name}"]
 
     with st.expander("DeepONet"):
@@ -214,25 +193,19 @@ def main():
         btn.write("\n")
         if btn.button("Infer", use_container_width=True, key=f"button_{model_name}"):
             model = load_model(model_name)
-            y = make_mesh(torch.linspace(*sess.xlim, Nx), torch.linspace(*sess.tlim, Nt))
-            u = sess.data[:: Nx // model.num_sensor, 0].unsqueeze(0)
-            preds = model((u, y)).view(Nx, Nt)
+            y = make_mesh(torch.linspace(*sess.xlim, Nx), torch.linspace(*sess.tlim, Nt)).cuda()
+            u = sess.data[:: sess.data.shape[0] // model.num_sensor, 0].unsqueeze(0).cuda()
+            st.write(Nx, y.size(), u.size())
+            preds = model((u, y)).view(Nx, Nt).detach().cpu()
+            st.write(preds.shape)
             # Plot result
             with st.spinner("Drawing Full Field..."):
-                web_figure("plot_solution", sess.generator.xs, sess.generator.ts, sess.data, preds)
+                web_figure("plot_solution", sess.xlim, sess.tlim, sess.data, preds)
             with st.spinner("Animating Solution..."):
-                web_animation(
-                    "animate_solution",
-                    sess.generator.xs,
-                    sess.data,
-                    sess.generator.ts,
-                    preds,
-                    save_img=False,
-                )
+                web_animation("animate_solution", sess.xlim, sess.tlim, sess.data, preds, save_img=False)
 
         del sess[f"Nx_{model_name}"], sess[f"Nt_{model_name}"], sess[f"button_{model_name}"]
-    st.write(sess.data.size())
-    st.write(sess)
+    st.sidebar.write(sess)
 
 
 if __name__ == "__main__":
